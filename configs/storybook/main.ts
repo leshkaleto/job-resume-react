25cba51f74effd28ffebd6f97a74874836691a5a
leto <aleksey.derevenskikh@robusta.team>
2024-02-05 21:09:25 +0300

    add storybook dark-mode-addon

--- configs/storybook/main.ts
+++ configs/storybook/main.ts
@@ -13,6 +13,7 @@ const config: StorybookConfig = {
         '@storybook/addon-essentials',
         '@storybook/addon-onboarding',
         '@storybook/addon-interactions',
+        'storybook-dark-mode',
     ],
     framework: {
         name: '@storybook/react-vite',
